cc37e2f88d5d3cfbe2b215c2bcdd8c9d1fcfc5ab
Niged01 <nigedoberer@gmail.com>
2022-02-08 14:26:59 +0000

    Change question arrays syntax for neater look and implemented the game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,170 +1,111 @@
-
 // variables
-const start = document.getElementById("start");
+
+// const start = document.getElementById("start");
 const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName("answer-text"));
+const answers = document.getElementsByClassName("answer-text");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let currentQuestionIndex = 0;
-score = 0;
+let score = 0;
 let availableQuestions= [];
 
 let nextQuestion = document.getElementById('next-question');
-let score = document.getElementById('score');
+let tally = document.getElementById('score');
 let incorrect = document.getElementById('incorrect');
 
 // Constants
 const MAX_NUMBER = 10;
+const QUESTIONS = [
+    {
+        "question": "Name this plane?",
+        "options": [ "Boeing 747", "Airbus A380", "Boeing 787", "Airbus A350"],
+        "correct": 4,
+        "imgSrc": "assets/images/a350.jpg"
+    },
+    {
+        "question": "What airline does this tail symbol belong to?",
+        "options": [ "American", "Lufthansa", "British Airways", "Qantas"],
+        "correct": 2,
+        "imgSrc": "url"
+    },
+    {
+        "question": "Airbus's most produced plane is the?",
+        "options": [ "A320", "A330", "A350", "A380" ],
+        "correct": 1,
+        "imgSrc": "url"
+    },
+    {
+        "question": "ILS to a pilot means?",
+        "options": [ "Information Localizer System", "Instrument Landing System", "Instrument Localizer System", "Information Landing System" ],
+        "correct": 2,
+        "imgSrc": "url"
+    },
+    {
+        "question": "Who makes the A380 engines?",
+        "options": [ "Boeing", "Land Rover", "Bentley", "Rolls-Royce"],
+        "correct": 4,
+        "imgSrc": "url"
+    },
+    {
+        "question": "Which one of these Aircraft has the longest flying capacity? ",
+        "options": [ "A350-900ULR", "747-800", "A380-300", "787 Dreamliner"],
+        "correct": 2,
+        "imgSrc": "url"
+    },
+    {
+        "question": "This is a cockpit of a?",
+        "options": [ "747", "A320", "A330", "777" ],
+        "correct": 3,
+        "imgSrc": "url"
+    },
+    {
+        "question": "What year was the 1st A380 produced?",
+        "options": [ "1990", "1997", "2007", "2010" ],
+        "correct": 3,
+        "imgSrc": "url"
+    },
+    {
+        "question": "who was the 1st woman to fly solo around the globe?",
+        "options": [ "Zara Rutherford", "Amelia Earhart", "Sarah Smith", "Jerrie Mock" ],
+        "correct": 2,
+        "imgSrc": "url"
+    },
+    {
+        "question": "Which Airline is the oldest?",
+        "options": [ "Korean", "Delta", "British Airways", "KLM" ],
+        "imgSrc": "url"
+    },
+]
 
+// Event Listener
 
 // Start quiz
 
 startGame = () => {
     currentQuestionIndex = 0;
     score = 0;
-    availableQuestions= [...questions];
-    renderNextQuestion()
+    availableQuestions= [...QUESTIONS];
+    renderNextQuestion();
 }
 
-function startGame()
-// event listeners
+//Quiz game
 
-
-
-// quiz questions
-const questions = [
-    {
-        "question": "Name this plane?",
-        "choice1": "Boeing 747",
-        "choice2": "Airbus A380",
-        "choice3": "Boeing 787",
-        "choice4": "Airbus A350",
-        "correct": 4,
-        "imgSrc": "assets/images/a350.jpg"
-    },
-    {
-        "question": "What airline does this tail symbol belong to?",
-        "choice1" : "American",
-        "choice2" : "Lufthansa",
-        "choice3" : "British Airways",
-        "choice4" : "Qantas",
-        "correct": 2,
-        "imgSrc": "url"
-    },
-    {
-        "question": "Airbus's most produced plane is the?",
-        "choice1" : "A320",
-        "choice2" : "A330",
-        "choice3" : "A350",
-        "choice4" : "A380",
-        "correct": 1,
-        "imgSrc": "url"
-    },
-    {
-        "question": "ILS to a pilot means?",
-        "choice1" : "Information Localizer System",
-        "choice2" : "Instrument Landing System",
-        "choice3" : "Instrument Localizer System",
-        "choice4" : "Information Landing System",
-        "correct": 2,
-        "imgSrc": "url"
-    },
-    {
-        "question": "Who makes the A380 engines?",
-        "choice1" : "Boeing",
-        "choice2" : "Landrover",
-        "choice3" : "Bently",
-        "choice4" : "Rolls-Royce",
-        "correct": 4,
-        "imgSrc": "url"
-    },
-    {
-        "question": "Which one of these Aircraft has the longest flying capacity? ",
-        "choice1" : "A350-900ULR",
-        "choice2" : "747-800",
-        "choice3" : "A380-300",
-        "choice4" : "787 Dreamliner",
-        "correct": 2,
-        "imgSrc": "url"
-    },
-    {
-        "question": "This is a cockpit of a?",
-        "choice1" : "747",
-        "choice2" : "A320",
-        "choice3" : "A330",
-        "choice4" : "777",
-        "correct": 3,
-        "imgSrc": "url"
-    },
-    {
-        "question": "What year was the 1st A380 produced?",
-        "choice1" : "1990",
-        "choice2" : "1997",
-        "choice3" : "2007",
-        "choice4" : "2010",
-        "correct": 3,
-        "imgSrc": "url"
-    },
-    {
-        "question": "who was the 1st woman to fly solo around the globe?",
-        "choice1" : "zara Rutherford",
-        "choice2" : "Amelia Earhart",
-        "choice3" : "Sarah Smith",
-        "choice4" : "Jerrie Mock",
-        "correct": 2,
-        "imgSrc": "url"
-    },
-    {
-        "question": "Which Airline is the oldest?",
-        "choice1" : "Korean",
-        "choice2" : "Delta",
-        "choice3" : "British Airways",
-        "choice4" : "KLM",
-        "correct": 4,
-        "imgSrc": "url"
-    },
-]
-
-
-
-
-// quiz game
 function renderNextQuestion() {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
-    }
     currentQuestionIndex++;
-    const questionIndex = math.floor(math.random () * availbleQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-    
-    answers.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number]
-    })
+    let nextQuestion = QUESTIONS[currentQuestionIndex];
 
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+    question.innerText = nextQuestion["question"]
+    for (i = 0; i<nextQuestion["options"].length; i++){
+        answers[i].innerText = nextQuestion["options"][i];
+    }
+
+    // hintImageTag.src = nextQuestion["imgSrc"];
 }
 
-answers.forEach( choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        renderNextQuestion()
-    })
-})
-
-function onUserSelection(option) {
-    let userSelection = option.text;
-    let currentQuestion = questions[currentQuestionIndex];
+function onUserSelection(clickEvent) {
+    let userSelection = clickEvent.target.dataset.number;
+    let currentQuestion = QUESTIONS[currentQuestionIndex];
     let correctOption = currentQuestion["correct"];
 
     if (userSelection == correctOption) {
@@ -176,11 +117,18 @@ function onUserSelection(option) {
     updateScore();
 }
 
-function updateScore() {
-    score++;
-    updateTallyBoard(score);
+function showPopUp() {
+    alert("correct answer");
 }
 
+function showAnotherPopUp() {
+    alert("incorrect answer");
+}
+
+function updateScore() {
+    score++;
+    // updateTallyBoard(score);
+}
 
 // scores area
 function incrementScore() {
@@ -192,3 +140,14 @@ function incrementScore() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
   }
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    startGame()
+});
+
+for (answer of answers) {
+    answer.addEventListener("click", onUserSelection);    
+}
+
+// answers.forEach(answer => {
+// });
