a049b309b30aa72df3ba440a08b61f0d5718a408
Niged01 <nigedoberer@gmail.com>
2022-02-09 15:19:44 +0000

    add images to questions array in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,58 +31,58 @@ const QUESTIONS = [
         "imgSrc": "assets/images/a350.jpg"
     },
     {
-        "question": "What airline does this tail symbol belong to?",
+        "question": "Who's tail does this belong to?",
         "options": [ "American", "Lufthansa", "British Airways", "Qantas"],
         "correct": 2,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/lufthansa.jpg"
     },
     {
         "question": "Airbus's most produced plane is the?",
         "options": [ "A320", "A330", "A350", "A380" ],
         "correct": 1,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/airbusfleet.jpg"
     },
     {
         "question": "ILS to a pilot means?",
         "options": [ "Information Localizer System", "Instrument Landing System", "Instrument Localizer System", "Information Landing System" ],
         "correct": 2,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/cockpit.jpg"
     },
     {
         "question": "Who makes the A380 engines?",
         "options": [ "Boeing", "Land Rover", "Bentley", "Rolls-Royce"],
         "correct": 4,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/a380engine.jpg"
     },
     {
-        "question": "Which one of these Aircraft has the longest flying capacity? ",
+        "question": "which has the longest flying capacity? ",
         "options": [ "A350-900ULR", "747-800", "A380-300", "787 Dreamliner"],
         "correct": 1,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/planes.jpg"
     },
     {
         "question": "This is a cockpit of a?",
         "options": [ "747", "A320", "A330", "777" ],
         "correct": 3,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/a330cockpit.jpg"
     },
     {
         "question": "What year was the 1st A380 produced?",
         "options": [ "1990", "1997", "2007", "2010" ],
         "correct": 3,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/airbusa380.jpg"
     },
     {
-        "question": "who was the 1st woman to fly solo around the globe?",
+        "question": "1st woman to fly solo around the globe?",
         "options": [ "Zara Rutherford", "Amelia Earhart", "Sarah Smith", "Jerrie Mock" ],
         "correct": 2,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/earhart.jpg"
     },
     {
         "question": "Which Airline is the oldest?",
         "options": [ "Korean", "Delta", "British Airways", "KLM" ],
         "correct": 4,
-        "imgSrc": "url"
+        "imgSrc": "assets/images/tailfin.jpg"
     },
 ]
 
@@ -108,7 +108,7 @@ function renderNextQuestion() {
         nextQuestionsRef.style.display = 'none'  
         finishRef.style.display = 'inline'  
     } else {
-        nextQuestionsRef.style.display ='inline'
+        //nextQuestionsRef.style.display ='inline'
         finishRef.style.display = 'none'
     }
     if (answerSelected) {
