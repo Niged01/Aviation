18135866b84518dfb2ec850c240f304f9babda28
Niged01 <nigedoberer@gmail.com>
2022-02-09 16:22:47 +0000

    Add final score code to highScores Javascript

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -2,6 +2,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     readScore()
 });
 
-function readScore() {
-      let score = localStorage.getItem('score');
+const usernameRef = document.querySelector('#username');
+const saveScoreRef= document.querySelector('#saveScoreBtn');
+const finalScoreRef = document.querySelector('#finalScore');
+const score = localStorage.getItem('score');
+
+
+
+usernameRef.addEventListener('keyup', () => {
+    saveScoreRef.disabled = !usernameRef.ariaValueMax;
+});
+
+finalScoreRef.innerText = score;
+
+saveHighScore = e => {
+    e.preventDefault();
 }
